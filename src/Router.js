8c67453b030a0006232ae7da62dd4ff6bd6585e7
shaiod <tltmdpffl@gmail.com>
2022-11-08 12:56:20 +0900

    kakao_app

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,4 @@
 
-import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Auth from './routes/Auth';
 import AuthForm from './components/AuthForm';
